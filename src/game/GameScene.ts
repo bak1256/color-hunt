156031757c9cf69971c1f419c548c727f6187e69
bak1256 <bak1256@gmail.com>
2026-08-22 01:47:41 +0900

    ui: tune lobby avatar size and position

--- src/game/GameScene.ts
+++ src/game/GameScene.ts
@@ -24850,7 +24850,7 @@ export class GameScene extends Phaser.Scene {
                  */
                 avatarImage.style.setProperty(
                     'transform',
-                    'translateY(-8%) scale(.78)',
+                    'translateY(-13%) scale(.858)',
                     'important',
                 );
                 avatarImage.style.setProperty(
